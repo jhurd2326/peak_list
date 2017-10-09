d9b16251342c8c0d7213905477aa73d9226832ae
jhurd2326 <jake.hurd@gmail.com>
2017-10-09 12:21:55 -0400

    Style the login and register pages

--- site/index.php
+++ site/index.php
@@ -13,8 +13,8 @@
     <link rel="stylesheet" href="stylesheets/custom.css" />
   </head>
   <body>
-    <div class="background flex-center">
-      <div class="container flex-center">
+    <div class="background">
+      <div class="container h-100">
         <?php
           if(isset($_GET["error"]))
           {
@@ -22,29 +22,33 @@
           }
         ?>
         <?php if(check_login($mysqli) == false): ?>
-          <div class="card card-cascade login-card animated fadeIn">
-            <div class="card-header default-color text-center">
-              <h2 class="h2-responsive" style="color: white;">Sign-In</h2>
-            </div>
-            <div class="card-body mx-4">
-              <form action="php/process_login.php" method="post" name="login_form">
-                <div class="md-form">
-                  <input type="text" name="username" id="username" class="form-control" />
-                  <label for="username">Username</label>
+          <div class="row flex-center">
+            <div class="col-lg-4 col-md-12 col-sm-12 col-xs-12">
+              <div class="card animated fadeIn">
+                <div class="card-header default-color text-center">
+                  <h2 class="h2-responsive" style="color: white;">Sign-In</h2>
                 </div>
-                <div class="md-form">
-                  <input type="password" name="password" id="password" class="form-control" />
-                  <label for="password">Password</label>
-                </div>
-                <div class="text-center my-3">
-                  <input type="button" value="Login" onclick="formhash(this.form, this.form.password);" class="btn btn-default"/>
-                </div>
-              </form>
+                <div class="card-body">
+                  <form action="php/process_login.php" method="post" name="login_form">
+                    <div class="md-form">
+                      <input type="text" name="username" id="username" class="form-control" />
+                      <label for="username">Username</label>
+                    </div>
+                    <div class="md-form">
+                      <input type="password" name="password" id="password" class="form-control" />
+                      <label for="password">Password</label>
+                    </div>
+                    <div class="text-center my-3">
+                      <input type="button" value="Login" onclick="formhash(this.form, this.form.password);" class="btn btn-default"/>
+                    </div>
+                  </form>
 
-              <p class="text-center my-3">
-                Don't have an account?
-                <a href='register.php' style="color: #2BBBAD">Register</a>
-              </p>
+                  <p class="text-center my-3">
+                    Don't have an account?
+                    <a href='register.php' style="color: #2BBBAD">Register</a>
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         <?php else: ?>
--- site/register.php
+++ site/register.php
@@ -6,44 +6,85 @@
 <html>
   <head>
     <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Register</title>
-    <script type="text/JavaScript" src="javascripts/sha512.js"></script>
-    <script type="text/JavaScript" src="javascripts/forms.js"></script>
+    <link rel="stylesheet" href="stylesheets/custom.css"  />
   </head>
   <body>
-    <h1>Register</h1>
-    <?php
-      if(!empty($error_msg)) { echo $error_msg; }
-    ?>
-    <ul>
-      <li>Usernames may contain only digits, upper and lowercase letters and underscores</li>
-      <li>Emails must have a valid email format</li>
-      <li>Passwords must be at least 6 characters long</li>
-      <li>Passwords must contain
-          <ul>
-              <li>At least one uppercase letter (A..Z)</li>
-              <li>At least one lowercase letter (a..z)</li>
-              <li>At least one number (0..9)</li>
-          </ul>
-      </li>
-      <li>Your password and confirmation must match exactly</li>
-    </ul>
-    <form action="php/process_registration.php" method="post" name="registration_form">
-      Username: <input type="text" name="username" id="username" /><br>
-      Password: <input type="password" name="password" id="password"/><br>
-      Confirm Password: <input type="password" name="confirmation" id="confirmation" /><br>
-      Email: <input type="text" name="email" id="email" /><br />
-      First Name: <input type="text" name="first_name" id="first_name" /><br />
-      Last Name: <input type="text" name="last_name" id="last_name" /><br />
-      Age: <input type="text" name="age" id="age" /><br />
-      Telephone: <input type="text" name="phone" id="phone" /><br />
-      Address: <input type="text" name="address" id="address" /><br />
-      
-      <input type="button" value="Register"
-        onclick="regformhash(this.form, this.form.username, this.form.password,
-                 this.form.confirmation, this.form.email, this.form.first_name,
-                 this.form.last_name, this.form.age, this.form.phone, this.form.address);" />
-    </form>
-    <p>Return to the <a href="index.php">login page</a>.</p>
+    <div class="background">
+      <div class="container h-100">
+        <?php if(!empty($error_msg)) { echo $error_msg; } ?>
+        <div class="row flex-center">
+          <div class="col-lg-4 col-md-12 col-sm-12 col-xs-12">
+            <div class="card animated fadeIn">
+              <div class="card-header default-color text-center">
+                <h2 class="h2-responsive" style="color: white;">Register</h2>
+              </div>
+              <div class="card-body">
+                <form action="php/process_registration.php" method="post" name="registration_form">
+                  <div class="md-form">
+                    <input type="text" name="email" id="email" />
+                    <label for="email">Email</label>
+                  </div>
+                  <div class="md-form">
+                    <input type="text" name="username" id="username" />
+                    <label for="username">Username</label>
+                    <p class="mt-1 inline-form-text">Digits, Letters, and Underscores Only</p>
+                  </div>
+                  <div class="md-form">
+                    <input type="password" name="password" id="password"/>
+                    <label for="password">Password</label>
+                    <p class="mt-1 inline-form-text">At least 6 characters in length</p>
+                  </div>
+                  <div class="md-form">
+                    <input type="password" name="confirmation" id="confirmation" />
+                    <label for="confirmation">Password Confirmation</label>
+                    <p class="mt-1 inline-form-text">Must match password exactly</p>
+                  </div>
+                  <div class="md-form">
+                    <input type="text" name="first_name" id="first_name" />
+                    <label for="first_name">First Name</label>
+                  </div>
+                  <div class="md-form">
+                    <input type="text" name="last_name" id="last_name" />
+                    <label for="last_name">Last Name</label>
+                  </div>
+                  <div class="md-form">
+                    <input type="text" name="age" id="age" />
+                    <label for="age">Age</label>
+                  </div>
+                  <div class="md-form">
+                    <input type="text" name="phone" id="phone" />
+                    <label for="phone">Telephone</lable>
+                  </div>
+                  <div class="md-form">
+                    <input type="text" name="address" id="address" />
+                    <label for="address">Address</label>
+                  </div>
+
+                  <div class="text-center my-3">
+                    <input type="button" value="Register" class="btn btn-default"
+                      onclick="regformhash(this.form, this.form.username, this.form.password,
+                               this.form.confirmation, this.form.email, this.form.first_name,
+                               this.form.last_name, this.form.age, this.form.phone, this.form.address);" />
+                  </div>
+                </form>
+                <p class="text-center my-3">
+                  Return to the
+                  <a href="index.php" style="color: #2BBBAD;">login page</a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <script type="text/JavaScript" src="javascripts/jquery-3.2.1.min.js"></script>
+    <script type="text/JavaScript" src="javascripts/sha512.js"></script>
+    <script type="text/JavaScript" src="javascripts/forms.js"></script>
+    <script type="text/JavaScript" src="javascripts/popper.min.js"></script>
+    <script type="text/JavaScript" src="javascripts/bootstrap.min.js"></script>
+    <script type="text/JavaScript" src="javascripts/mdb.min.js"></script>
   </body>
 </html>
