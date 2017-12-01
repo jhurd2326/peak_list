76bf6d680e536893b524b633b990c664b2957690
jhurd2326 <jake.hurd@gmail.com>
2017-12-01 15:29:17 -0500

    Fix mysqldump to use absolute path and reimplement db restore/save

--- site/admin/restore.php
+++ site/admin/restore.php
@@ -2,10 +2,14 @@
   include_once "../php/db_connect.php";
   include_once "../php/functions.php";
 
+  if(isset($_GET["file"]))
+    $file = $_GET["file"];
+  else
+    header("Location: backup.php");
+
   if( check_login($dbh) && check_admin($_SESSION["user_id"], $dbh) )
   {
-    $command = "/usr/bin/mysqldump --host={$db_host} --password={$db_password} --user={$db_username} ".
-               "--databases {$db_name} > ../backups/backup-" . date("YdmHis") . ".sql";
+    $command = "mysql -h {$db_host} -u {$db_username} --password={$db_password} {$db_name} < ../backups/" . $file;
     exec($command);
 
     header("Location: backup.php");
